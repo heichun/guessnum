1c440c60a65763cf488455d44d4d7be5b958a2bc
Floyd wong <heichun1@gmail.com>
2022-08-06 22:48:33 +0800

    add count

--- num.py
+++ num.py
@@ -1,6 +1,17 @@
 import random
 
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1 #count = count + 1
 	num = input('no?')
 	num = int(num)
+	if num == r:
+		print('you right!')
+		print('你估左', count, '次')
+		break
+	elif num > r:
+		print('細D')
+	elif num < r:
+		print('大D')
+	print('你估左', count, '次')
